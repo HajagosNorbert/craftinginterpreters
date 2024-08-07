88b260bf0602b052202a2910e56bb206e01e2fd9
Norbert Hajagos <hajagosnorbi@gmail.com>
2024-08-07 22:11:33 +0200

    saved before maybe switching from nixos

--- rlox/src/main.rs
+++ rlox/src/main.rs
@@ -1,22 +1,23 @@
 use std::fmt;
 use std::vec;
 
-mod Lox {
-    #[derive(Debug)]
-    pub enum OpCode {
-        OpReturn,
-    }
-
-    // impl fmt::Debug for Vec<OpCode> {
-    //     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    //         f.debug_struct("Vec").field("buf", &self.buf).field("len", &self.len).finish()
-    //     }
-    // }
+#[derive(Debug)]
+enum OpCode {
+    OpReturn,
 }
 
+struct Chunk {
+    codes: Vec<OpCode>
+}
+
+// impl fmt::Debug for Vec<OpCode> {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         f.debug_struct("Vec").field("buf", &self.buf).field("len", &self.len).finish()
+//     }
+// }
 
 fn main() {
-    let mut chunk: Vec<Lox::OpCode> = Vec::new();
+    let mut chunk: Vec<OpCode> = Vec::new();
     // chunk.push(OpCode::OpReturn);
     // chunk.push(OpCode::OpReturn);
     println!("{:?}", chunk);
